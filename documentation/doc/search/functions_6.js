4279ef9bec7d4ce8924ccf6893881df8fbe37c39
davidcardoso-ti <davidcardoso.ti@gmail.com>
2016-12-11 20:29:27 -0300

    11/12/2016 20h25 - OK: filtro de animais por classe, veterinario e tratador - OK: exportando resultado após filtros em um arquivo de exportação

--- documentation/doc/search/functions_6.js
+++ documentation/doc/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['main',['main',['../main_8cpp.html#af3ed9c200de85b53c94cd18764b246a2',1,'main.cpp']]],
+  ['main',['main',['../exportar_8cpp.html#af3ed9c200de85b53c94cd18764b246a2',1,'main(int argc, char *const argv[]):&#160;exportar.cpp'],['../main_8cpp.html#af3ed9c200de85b53c94cd18764b246a2',1,'main(int argc, char *const argv[]):&#160;main.cpp']]],
   ['mamifero',['Mamifero',['../classMamifero.html#a606df37a2b0f025ba2717138b491cef4',1,'Mamifero::Mamifero(int id, string classe, string batismo)'],['../classMamifero.html#a65a2007141f78e2c233eab34cd0390cc',1,'Mamifero::Mamifero(int, string, string, string, string, string, char, float, Pessoa *, Pessoa *, string)']]],
   ['mamiferoexotico',['MamiferoExotico',['../classMamiferoExotico.html#af007dbbdaaa12f153ca39c8e402f9af5',1,'MamiferoExotico::MamiferoExotico(int id, string classe, string batismo)'],['../classMamiferoExotico.html#a6700cc57ed4ce0a3ed93fc1b88bb22f4',1,'MamiferoExotico::MamiferoExotico(int, string, string, string, string, string, char, float, Pessoa *, Pessoa *, string, string, string)']]],
   ['mamiferonativo',['MamiferoNativo',['../classMamiferoNativo.html#a49d79b2b4959ffbb249b591e086266ec',1,'MamiferoNativo::MamiferoNativo(int id, string classe, string batismo)'],['../classMamiferoNativo.html#ac4f7bc15fbd0b333273953aea84ac8e3',1,'MamiferoNativo::MamiferoNativo(int, string, string, string, string, string, char, float, Pessoa *, Pessoa *, string, string, string, string)']]]
--- src/petfera/exportar.cpp
+++ src/petfera/exportar.cpp
@@ -12,6 +12,7 @@
   */
 
 #include <iostream>     // biblioteca de entrada e saída do C++
+#include <sstream>     // biblioteca de entrada e saída do C++
 #include <fstream>    	// biblioteca de manipulação de arquivos do C++
 #include "../../include/petfera/auxiliar.h" // funções auxiliares
 
@@ -27,9 +28,9 @@ int main( int argc, char* const argv[] ){
 	cout << endl;
 	cout << "######## Programa Exportar Pet Fera ########" << endl << endl;
 
-	string path_pessoas = "data/petfera/pessoas.csv"; /**< path_pessoas - caminho do arquivo de pessoas */
-	string path_animais = "data/petfera/animais.csv"; /**< path_animais - caminho do arquivo de animais */
-	string path_exportar = "data/petfera/exportar.csv"; /**< path_exportar - caminho do arquivo para animais exportados */
+	string path_pessoas = "data/petfera/pessoas.csv"; /**< path_pessoas - arquivo de pessoas */
+	string path_animais = "data/petfera/animais.csv"; /**< path_animais - arquivo de animais */
+	string path_exportar = "data/petfera/exportar.csv"; /**< path_exportar - arquivo para animais exportados */
 
 	map<int, Pessoa*> pessoas; /**< pessoas - dicionario de objetos Pessoa */
 	map<int, Animal*> animais; /**< animais - dicionario de objetos Animal */
@@ -37,17 +38,57 @@ int main( int argc, char* const argv[] ){
 	int pessoas_last_id = 0; /**< pessoas_last_id - ultimo ID de pessoas */
 	int animais_last_id = 0; /**< animais_last_id - ultimo ID de animais */
 
-	int user_option = 0; /**< user_option - opção escolhida pelo usuario */
+	pair<int, string> arg_classe(0, ""); 		/**< arg_classe - posicao e valor do argumento */
+	pair<int, string> arg_veterinario(0, ""); 	/**< arg_veterinario - posicao e valor do argumento */
+	pair<int, string> arg_tratador(0, ""); 		/**< arg_tratador - posicao e valor do argumento */
 
 	// valida parametros passados via linha de comando
-	if(argc < 2 || argc > 9) {
+	if(argc != 2 && argc != 4 && argc != 6 && argc != 8) {
 		cerr << "Erro: quantidade de parametros incorreta!" << endl;
+		cout << "Exemplo de chamada correta:" << endl 
+			 <<"\t./exportar.exe -c <classe> -v <veterinaro> -t <tratador> <arquivo_exportar>" << endl;
 		return -1;
+	}else{
+		path_exportar = argv[argc-1]; // ultimo argumento
+
+		if (argc >= 4) // tem argumentos opcionais (-c, -v, -t)
+		{
+			int i = 1;
+			while(i < argc-1)
+			{
+				string ss = argv[i]; /**< ss - converte para string o valor do argumento para comparar adequadamente */
+				if (ss == "-c")
+				{
+					arg_classe.first  = i;
+					arg_classe.second = argv[i+1];
+				}else
+				if (ss == "-v")
+				{
+					arg_veterinario.first  = i;
+					arg_veterinario.second = argv[i+1];
+				}else
+				if (ss == "-t")
+				{
+					arg_tratador.first  = i;
+					arg_tratador.second = argv[i+1];
+				}
+
+				i+=2;
+			}
+
+			if (arg_classe.first == 0 && arg_veterinario.first == 0 && arg_tratador.first == 0){
+				cerr << "Erro: parametros opcionais incorretos!" << endl;
+				cout << "Exemplo de chamada correta:" << endl 
+					 <<"\t./exportar.exe -c <classe> -v <veterinaro> -t <tratador> <arquivo_exportar>" << endl;
+				return -1;
+			}
+		}
+		
 	}
 
 	cout << "Atenção: os dados serão salvos em:" << endl;
 	cout << path_exportar << endl;
-	cout << endl;
+	cout << endl << endl;
 
 	// preenche dicionario de pessoas
 	fillPessoas(pessoas, path_pessoas, pessoas_last_id);
@@ -55,34 +96,34 @@ int main( int argc, char* const argv[] ){
 	// preenche dicionario de animais
 	fillAnimais(pessoas, animais, path_animais, animais_last_id);
 
-	// tratamento das combinações dos paramêtros
-	cout << endl;
 
-	if(user_option){
-
-		switch(user_option){
-
-			case 1: // pesquisar pessoa
-				searchPessoa(pessoas);
-			break;
-
-			case 2: // pesquisar animal
-				searchAnimal(animais);
-			break;
-
-			default:
-				cout << "Sorry! Opção não implementada! =/" << endl;
-
-		}
-
+	// realiza filtros no dicionario de animais antes de exportar
+	if (arg_classe.first != 0){
+		if (filterExport(pessoas, animais, "-c", arg_classe.second) )
+			cout << "Filtro de classe realizado com sucesso!" << endl;
+		else
+			cout << "Sem ocorrencias para a classe informada!" << endl;
+	}
+	if (arg_veterinario.first != 0){
+		if (filterExport(pessoas, animais, "-v", arg_veterinario.second) )
+			cout << "Filtro de veterinario realizado com sucesso!" << endl;
+		else
+			cout << "Sem ocorrencias para o veterinario informado!" << endl;
+	}
+	if (arg_tratador.first != 0){
+		if (filterExport(pessoas, animais, "-t", arg_tratador.second) )
+			cout << "Filtro de tratador realizado com sucesso!" << endl;
+		else
+			cout << "Sem ocorrencias para o tratador informado!" << endl;
 	}
 
-	// exporta dados pesquisados para um arquivo
-	if ( export2File(pessoas, animais, path_pessoas, path_animais) ){
+
+	// exporta dados para um arquivo
+	if ( export2File(pessoas, animais, path_exportar) ){
 		cout << endl;
-		cout << "Dados salvos no arquivo:" << endl;
-		cout << path_pessoas << endl;
-		cout << endl;
+		cout << "Resultado: " << animais.size() << endl 
+			 << "Dados salvos no arquivo:" << endl
+			 << path_exportar << endl;
 
 		// limpa os dicionarios em memoria
 		pessoas.clear();
